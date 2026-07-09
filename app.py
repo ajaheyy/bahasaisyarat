5e882c3db284a45046c9e777195b07505ffb1d48
Vindiar Putra <vindiarputra02@gmail.com>
2026-07-09 21:47:12 +0700

    switch deployment target to Hugging Face Spaces (add Dockerfile, optimize requirements.txt for CPU torch, change default port to 7860, remove railway.toml)

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def predict_frame():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5050))
+    port = int(os.environ.get("PORT", 7860))
     app.run(debug=False, host="0.0.0.0", port=port)
